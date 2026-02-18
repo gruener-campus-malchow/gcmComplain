c10ca2269b1ce2897667a0fed0854eeea0149a9c
IcyPebble <g2sr90@gmail.com>
2026-02-18 08:46:31 +0100

    sql

--- datenbank.sql
+++ datenbank.sql
@@ -1,5 +1,3 @@
-datenbank
-
 CREATE TABLE
     nachricht
         id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -7,5 +5,18 @@ CREATE TABLE
         text VARCHAR(200) NOT NULL,
         FOREIGN KEY (mail) REFERENCES person(mail),
         FOREIGN KEY (id) REFERENCES beschwerde(id);
+
+CREATE TABLE datei (
+  id varchar(100) NOT NULL,
+  datum datetime NOT NULL,
+  url varchar(100) NOT NULL,
+  typ varchar(100) NOT NULL,
+  endung varchar(100) NOT NULL,
+  name varchar(100) NOT NULL,
+  nachricht_id varchar(100) NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (nachricht_id) REFERENCES nachricht(id)
+);
+   
 
 
