5ccde98f6c4aaae17cb1be590936d23af99f1da0
ijontychie <baldauf.a@berlin.gcm.schule>
2026-02-18 09:39:02 +0100

    debugging SQL-Code

--- src/datenbank.sql
+++ src/datenbank.sql
@@ -1,10 +1,37 @@
-CREATE TABLE
-    nachricht
+CREATE TABLE IF NOT EXISTS person (
+    person_email VARCHAR(100) NOT NULL,
+    PRIMARY KEY (person_email)
+);
+
+CREATE TABLE beschwerde(
+    id varchar(100) NOT NULL,
+    titel VARCHAR(100) NOT NULL,
+    zeit TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    reaktionszeit INT NOT NULL,
+    person_email VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (person_email)
+        REFERENCES person(person_email)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE person_beschwerde (
+    person_email VARCHAR(100) NOT NULL,
+    beschwerde_id INT NOT NULL,
+    PRIMARY KEY (person_email, beschwerde_id),
+    FOREIGN KEY (person_email) REFERENCES person(person_email),
+    FOREIGN KEY (beschwerde_id) REFERENCES beschwerde(id)
+    );
+
+CREATE TABLE nachricht (
         id INTEGER AUTO_INCREMENT PRIMARY KEY,
-        zeit TIMESTAMP NOW,
-        text VARCHAR(200) NOT NULL,
-        FOREIGN KEY (mail) REFERENCES person(mail),
-        FOREIGN KEY (id) REFERENCES beschwerde(id);
+        zeit TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        txt VARCHAR(200) NOT NULL,
+    	mail VARCHAR(200) NOT NULL,
+        FOREIGN KEY mail REFERENCES person(person_email),
+        FOREIGN KEY id REFERENCES beschwerde(id)
+);
 
 CREATE TABLE datei (
   id varchar(100) NOT NULL,
@@ -17,27 +44,3 @@ CREATE TABLE datei (
   PRIMARY KEY (id),
   FOREIGN KEY (nachricht_id) REFERENCES nachricht(id)
 );
-   
-CREATE TABLE person (
-    person_email VARCHAR (100) NOT NULL,
-    PRIMARY KEY (person_email)
-);
-
-
-
-CREATE TABLE person_beschwerde (
-    person_email VARCHAR(100) NOT NULL,
-    beschwerde_id INT NOT NULL,
-
-    PRIMARY KEY (person_email, beschwerde_id),
-
-    FOREIGN KEY (person_email)
-        REFERENCES Person(email)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
-
-    FOREIGN KEY (beschwerde_id)
-        REFERENCES Beschwerde(beschwerde_id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-
