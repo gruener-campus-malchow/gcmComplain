4218d7ae3bbcd4f171914371849aac0d9481145f
kerajaiai <jj@fija.de>
2026-02-18 08:45:47 +0100

    sql aktualisiert

--- datenbank.sql
+++ datenbank.sql
@@ -1,1 +1,11 @@
 datenbank
+
+CREATE TABLE
+    nachricht
+        id INTEGER AUTO_INCREMENT PRIMARY KEY,
+        zeit TIMESTAMP NOW,
+        text VARCHAR(200) NOT NULL,
+        FOREIGN KEY (mail) REFERENCES person(mail),
+        FOREIGN KEY (id) REFERENCES beschwerde(id);
+
+
