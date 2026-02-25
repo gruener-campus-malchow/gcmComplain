6ab7763984d6e45c8c2afd6deb7f4acc8d8170b2
ijontychie <baldauf.a@berlin.gcm.schule>
2026-02-25 08:03:15 +0100

    configs in db debugged

--- src/install.php
+++ src/install.php
@@ -4,4 +4,4 @@ require_once("config.php");
 require_once("Database.php");
 
 
-$databaseobject = new Database("DB_HOST", "DB_USER", "DB_PASSWORD","DB_NAME");
+$databaseobject = new Database(DB_HOST, DB_USER, DB_PASSWORD,DB_NAME);
