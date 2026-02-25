8b06633b2d317c64f9e8030fb69c18b64291b886
IcyPebble <g2sr90@gmail.com>
2026-02-25 09:06:51 +0100

    added init sql query

--- src/install.php
+++ src/install.php
@@ -5,3 +5,10 @@ require_once("Database.php");
 
 
 $databaseobject = new Database(DB_HOST, DB_USER, DB_PASSWORD,DB_NAME);
+
+try {
+	$sql_init_database = file_get_contents("datenbank.sql");
+	$databaseobject->query($sql_init_database);
+} catch (Exception $e) {
+	echo 'Failed to initiate database: ' . $e->getMessage();
+}
