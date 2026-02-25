4abc052401c59bab68adcf1fb5fe70ec5aa0d2cb
ijontychie <baldauf.a@berlin.gcm.schule>
2026-02-25 09:10:15 +0100

    css einbinden in installer

--- src/install.php
+++ src/install.php
@@ -5,3 +5,25 @@ require_once("Database.php");
 
 
 $databaseobject = new Database(DB_HOST, DB_USER, DB_PASSWORD,DB_NAME);
+
+echo '<!DOCTYPE html><html lang="de">
+    <head>
+        <link rel="stylesheet" href="https://gcm.schule/index.css">
+        <title>Install</title>
+    </head>
+<body>
+    <h1 class="cis-header">Installation</h1>';
+
+
+
+try{
+    $sqlcode=file_get_contents('datenbank.sql');
+}
+catch(Exception $e)
+{
+    echo 'Hier ist ein Fehler aufgetreten: '.$e;
+}
+
+echo '<textarea rows="20" cols="100">'.$sqlcode.'</textarea>';
+
+echo"</body></html>";
