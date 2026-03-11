d120086d93559096ac15555163e4671d2bc8a97c
Mika <mika.dankleff@gmail.com>
2026-03-11 09:42:25 +0100

    beschwerde posten

---
+++ src/api/sendAPI/index.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+
+require_once("../../lib/Database.php");
+
+//$json .= $_SERVER['REQUEST_METHOD'];
+
+$datenbank = new Database();
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	 $ergebnis = $datenbank->query("INSERT INTO `beschwerde` (`id`, `titel`, `zeit`, `reaktionszeit`, `person_email`) VALUES ('2', 'beschwrde tietel 2', current_timestamp(), '2', 'test@test.com');");    
+    $json = json_encode($ergebnis);     
+}else if ($_SERVER['REQUEST_METHOD'] == 'GET'){
+    $json = "fioio";
+}else{
+    $json = "Kein Plan!";
+}
+
+echo $json;
