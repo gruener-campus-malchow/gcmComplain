d797b6b6c8b50c584f0dd5f7ba7083024aa391eb
Phinix7777777 <flxklose@gmail.com>
2026-03-25 09:09:46 +0100

    merge

--- src/upgrade.php
+++ src/upgrade.php
@@ -3,10 +3,10 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once("./lib/Database.php");
+require_once("src/lib/Database.php");
 
 
-$databaseobject = new Database("./config.php");
+$databaseobject = new Database("src/config.php");
 
 try {
     $sql_init_database = file_get_contents("datenbank.sql");
