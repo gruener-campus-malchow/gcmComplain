bbe6f9ae0950febd71adf45f224831f685751084
Mika <mika.dankleff@gmail.com>
2026-03-25 09:38:46 +0100

    bals

--- src/api/sendAPI/index.php
+++ src/api/sendAPI/index.php
@@ -7,7 +7,6 @@ require_once("../../lib/Database.php");
 
 $datenbank = new Database();
 
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	 $ergebnis = $datenbank->query("INSERT INTO `beschwerde` (`id`, `titel`, `zeit`, `reaktionszeit`, `person_email`) VALUES ('2', 'beschwrde tietel 2', current_timestamp(), '2', 'test@test.com');");    
     $json = json_encode($ergebnis);     
