ac7246e0eac346fe3367c0c8f06293a8e383fe09
ijontychie <baldauf.a@berlin.gcm.schule>
2026-03-25 08:47:50 +0100

    added_upgrade

---
+++ src/upgrade.php
@@ -0,0 +1,18 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once("./lib/Database.php");
+
+
+$databaseobject = new Database("./config.php");
+
+try {
+    $sql_init_database = file_get_contents("datenbank.sql");
+    $databaseobject->query($sql_init_database);
+} catch (Exception $e) {
+    echo 'Failed to initiate database: ' . $e->getMessage();
+}
+
+echo '<textarea rows="20" cols="100">'.$sql_init_database.'</textarea>';
